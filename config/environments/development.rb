6a6f2850d940d79402bbc5fcd51d146d6c5fb1f7
syj121 <2854315596@qq.com>
2019-02-12 17:24:21 +0800

    bootstrap

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,4 +58,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  #新增
+  config.assets.digest = false
 end
---
+++ vendor/assets/javascripts/vendor_main.js
@@ -0,0 +1,3 @@
+//= require_tree .
+//= require bootstrap.min
+//= require jquery-2.1.1
